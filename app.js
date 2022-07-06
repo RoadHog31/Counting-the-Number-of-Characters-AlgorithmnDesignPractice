eec9b7ef8f11fe4e09d14514f264eeca30e4be85
Your Name <you@example.com>
2022-07-06 17:29:48 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// 2. Create a program that prompts for an input string and displays output that shows the input string and the number of
+// characters the string contains
+
+// nouns = input, string, characters
+// verbs = prompt, displays, output,
+
+
+//npm install prompt-sync
+const prompt = require('prompt-sync')();
+
+//Assignment
+let inputString = prompt(`What is the input string?` + ` `);
+
+
+//Selection
+if (inputString !== null && inputString !== '') {
+    
+    console.log(`${inputString} has ${inputString.length} characters`);
+}
+else {
+
+    //Iteration
+    if (inputString === null || inputString === '') {
+
+         let inputString = prompt(`No string or characters have been entered?` + ` `);
+        // console.log(`No string or characters have been entered`);
+        console.log(`${inputString} has ${inputString.length} characters`);
+        
+    }
+
+      
+}
+
+
